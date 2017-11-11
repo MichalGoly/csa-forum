55569ab5cd9aec24657cc3e56872f55fe767d697
MichalGoly <me@michalgoly.com>
2017-11-11 13:04:26 +0000

    Production deployment to Heroku from Circle CI build of a master branch

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
 echo "Deploying to Heroku..."
+
+wget -qO- https://cli-assets.heroku.com/install-ubuntu.sh | sh
+export HEROKU_DEBUG=1
+heroku plugins:install heroku-container-registry
+heroku container:login
+heroku container:push web --app mwg2forum
+sleep 10
+heroku run rake db:migrate --app mwg2forum
