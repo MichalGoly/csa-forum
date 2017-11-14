477cc8aaae5467184337eb2ff01b515d56305119
MichalGoly <me@michalgoly.com>
2017-11-14 23:09:40 +0000

    started to implement the topics/show view

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,18 @@ class Post < ApplicationRecord
   belongs_to :topic
   belongs_to :parent, class_name: "Post", :optional => true
   has_many :children, class_name: "Post", foreign_key: :parent_id
+
+  # Recursively calculates the number of nodes above this one
+  def number_of_parents
+    if self.is_parent
+      return 0
+    else
+      return 1 + self.parent.number_of_parents
+    end
+  end
+
+  # https://swaac.tamouse.org/rails/2015/12/15/self-referential-models-categories/
+  def is_parent
+    !!self.parent.nil?
+  end
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,25 @@
 class Topic < ApplicationRecord
   belongs_to :user
+  has_many :posts
+
+  def post_wrappers
+    # 1. Iterate over all posts of the topic
+    # 2. Recursively find the number of parent nodes for each Post
+    # 3. Set the number as the offset, add to out
+    # 4. Return out
+    out = Array.new
+    self.posts.each do |p|
+      offset = p.number_of_parents
+      out.push(PostWrapper.new(p, offset))
+    end
+    return out
+  end
+end
+
+class PostWrapper
+  attr_accessor :post, :offset
+  def initialize(post, offset)
+    @post = post
+    @offset = offset
+  end
 end
