1aeeebdd7e6c6e38a6bedad53a3f472ded16c894
MichalGoly <me@michalgoly.com>
2017-11-17 18:00:23 +0000

    added a basic cucumber test and modified the code slightly to make it pass

---
+++ features/step_definitions/post_steps.rb
@@ -0,0 +1,38 @@
+Given("that user {string} with password {string} has logged in") do |user, password|
+  visit("session/new")
+  fill_in("Login", :with => user)
+  fill_in("Password", :with => password)
+  click_button("Logon")
+end
+
+When("the user creates a new anonymous thread with the title {string} with the body {string}") do |title, body|
+  visit("/topics")
+  click_link("create-thread")
+  fill_in("title", :with => title)
+  fill_in("body", :with => body)
+  check("anonymous")
+  click_button("Submit")
+  visit("/topics")
+end
+
+Then("the current page should contain a new row containing the data:") do |table|
+  # https://gist.github.com/greis/1711522
+  table_element = find('table#topics-table')
+  headers = table_element.all('thead th').map(&:text)
+  # remove Date and the empy header on the very right
+  headers.delete_at(5)
+  headers.delete_at(0)
+  results = table_element.all('tbody tr').map do |tr|
+    {}.tap do |hash|
+      tds = tr.all('td').map(&:text)
+      # remove the timestamp, show and destroy links
+      tds.delete_at(6)
+      tds.delete_at(5)
+      tds.delete_at(0)
+      headers.each_with_index do |header, i|
+        hash[header] = tds[i]
+      end
+    end
+  end
+  table.diff!(results)
+end
--- features/support/env.rb
+++ features/support/env.rb
@@ -51,8 +51,12 @@ end
 #   end
 #
 
+# Seed the test database before running cucumber
+Before do |scenario|
+    load Rails.root.join('db/seeds.rb')
+end
+
 # Possible values are :truncation and :transaction
 # The :transaction strategy is faster, but might give you threading problems.
 # See https://github.com/cucumber/cucumber-rails/blob/master/features/choose_javascript_database_strategy.feature
 Cucumber::Rails::Database.javascript_strategy = :truncation
-
