a0b16c7c75ec89bc8ba0ae9bbdd7eb757f68fc92
MichalGoly <me@michalgoly.com>
2017-11-18 13:15:35 +0000

    added features and bare steps

---
+++ features/step_definitions/post_delete_steps.rb
@@ -0,0 +1,23 @@
+Given("a new thread titled {string} with body {string} is created") do |string, string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given("an anonymous thread titled {string} with body {string} is created") do |string, string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given("user goes to the forum page") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("he should be able to delete his thread titled {string}") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("he should not be able to delete the thread titled {string}") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("he should be able to delete all threads") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
